f30a4a7530113ed11f26c37aa13481f4646e69b2
Kelina Shrestha <kylesth@gmail.com>
2019-03-19 13:11:32 +0545

    Redesign experience timeline.

--- src/components/ExperienceTimeline.vue
+++ src/components/ExperienceTimeline.vue
@@ -1,52 +1,32 @@
 <template>
-  <section
-    id="experience-timeline"
-    class="hide-overflow"
-  >
-    <v-layout>
-      <v-flex
-        xs12
-        md12
-        deep-orange darken-4
-        text-xs-center
-        pa-5
-        white--text
+  <v-container grid-list-xl mt-4>
+    <h4 class="mb-5 display-1 text-xs-center font-weight-bold text-uppercase">
+      Experience
+    </h4>
+    <v-timeline>
+      <v-timeline-item
+        v-for="(experience, i) in experiences"
+        :key="i"
+        :color="experience.color"
+        small
       >
-        <base-bubble-1
-          style="transform: rotate(180deg) translateX(5%)"
-        />
-        <base-heading>
-          Experience
-        </base-heading>
+        <template v-slot:opposite>
+          <span
+            :class="`headline font-weight-bold ${experience.color}--text`"
+            v-text="experience.year"
+          ></span>
+        </template>
+        <div class="py-3">
+          <h2 :class="`headline font-weight-light mb-3 ${experience.color}--text`">{{ experience.title }}</h2>
+          <h3 :class="`sub-heading font-weight-bold mb-3 ${experience.color}--text`">{{ experience.company }}</h3>
+          <div>
+            {{ experience.description }}
+          </div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 
-        <base-text class="mb-5">
-          <v-timeline>
-            <v-timeline-item
-              v-for="experience in experiences"
-              :key="experience"
-              color="red lighten-2"
-              large
-            >
-              <template v-slot:opposite>
-                <span>{{ experience.year }}</span>
-              </template>
-              <v-card class="elevation-2">
-                <v-card-title primary-title>
-                  <div>
-                    <div class="headline">{{ experience.title }}</div>
-                    <span class="grey--text">{{ experience.company}}</span>
-                  </div>
-                </v-card-title>
-                <v-card-text>
-                  {{ experience.description }}
-                </v-card-text>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-        </base-text>
-      </v-flex>
-    </v-layout>
-  </section>
 </template>
 
 <script>
@@ -57,36 +37,42 @@ export default {
         title: 'Freelance Remote Software Engineer',
         company: 'KUDO Inc.',
         year: 'July 2018 - Present',
+        color: 'cyan',
         description: 'Develop rapid application features, stabilize and manage application codes independently.',
       },
       {
         title: 'Founder / Product Manager',
         company: 'BazaarFactory',
         year: 'July 2017 - Present',
+        color: 'green',
         description: 'Create and implement effective solutions to e-commerce platforms’ pain-points.',
       },
       {
         title: 'Software Engineer',
         company: 'WhiteHat Engineering',
         year: 'July 2017 - Oct 2017',
+        color: 'pink',
         description: 'Maintain complete product flow in React based web application.',
       },
       {
         title: 'Associate Software Engineer',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'July 2016 - July 2017',
+        color: 'orange',
         description: 'Maintain complete product flow in React based web application.',
       },
       {
         title: 'Junior Software Engineer',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'June 2015 - July 2016',
+        color: 'orange',
         description: 'Maintain complete product flow in React based web application.',
       },
       {
         title: 'Software Engineer Trainee',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'Jan 2015 - June 2016',
+        color: 'orange',
         description: 'Maintain complete product flow in React based web application.',
       }
     ]
