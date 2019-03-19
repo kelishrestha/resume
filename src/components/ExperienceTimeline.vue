cebfc6f5f2eebe1a873f1cde7be8fd60a863e239
Kelina Shrestha <kylesth@gmail.com>
2019-03-20 00:07:53 +0545

    Fix eslint warnings and color.

--- src/components/ExperienceTimeline.vue
+++ src/components/ExperienceTimeline.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container grid-list-xl mt-4>
+  <v-container
+    grid-list-xl
+    mt-4
+  >
     <h1 class="mb-5 display-2 text-xs-center font-weight-bold text-uppercase teal--text">
       Experience
     </h1>
@@ -14,11 +17,15 @@
           <span
             :class="`headline font-weight-bold ${experience.color}--text`"
             v-text="experience.year"
-          ></span>
+          />
         </template>
         <div class="py-3">
-          <h2 :class="`headline font-weight-light mb-3 ${experience.color}--text`">{{ experience.title }}</h2>
-          <h3 :class="`sub-heading font-weight-bold mb-3 ${experience.color}--text`">{{ experience.company }}</h3>
+          <h2 :class="`headline font-weight-light mb-3 ${experience.color}--text`">
+            {{ experience.title }}
+          </h2>
+          <h3 :class="`sub-heading font-weight-bold mb-3 ${experience.color}--text`">
+            {{ experience.company }}
+          </h3>
           <div>
             {{ experience.description }}
           </div>
@@ -26,7 +33,6 @@
       </v-timeline-item>
     </v-timeline>
   </v-container>
-
 </template>
 
 <script>
@@ -38,42 +44,42 @@ export default {
         company: 'KUDO Inc.',
         year: 'July 2018 - Present',
         color: 'cyan',
-        description: 'Develop rapid application features, stabilize and manage application codes independently.',
+        description: 'Develop rapid application features, stabilize and manage application codes independently.'
       },
       {
         title: 'Founder / Product Manager',
         company: 'BazaarFactory',
         year: 'July 2017 - Present',
         color: 'green',
-        description: 'Create and implement effective solutions to e-commerce platforms’ pain-points.',
+        description: 'Create and implement effective solutions to e-commerce platforms’ pain-points.'
       },
       {
         title: 'Software Engineer',
         company: 'WhiteHat Engineering',
         year: 'July 2017 - Oct 2017',
-        color: 'pink',
-        description: 'Maintain complete product flow in React based web application.',
+        color: 'orange',
+        description: 'Maintain complete product flow in React based web application.'
       },
       {
         title: 'Associate Software Engineer',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'July 2016 - July 2017',
-        color: 'orange',
-        description: 'Maintain complete product flow in React based web application.',
+        color: 'pink',
+        description: 'Maintain complete product flow in React based web application.'
       },
       {
         title: 'Junior Software Engineer',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'June 2015 - July 2016',
-        color: 'orange',
-        description: 'Maintain complete product flow in React based web application.',
+        color: 'pink',
+        description: 'Maintain complete product flow in React based web application.'
       },
       {
         title: 'Software Engineer Trainee',
         company: 'CloudFactory (Sprout Technologies)',
         year: 'Jan 2015 - June 2016',
-        color: 'orange',
-        description: 'Maintain complete product flow in React based web application.',
+        color: 'pink',
+        description: 'Maintain complete product flow in React based web application.'
       }
     ]
   })
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-container grid-list-xl fluid pa-0>
+  <v-container
+    grid-list-xl
+    fluid
+    pa-0
+  >
     <v-parallax
       dark
       :src="bgimage"
@@ -9,15 +13,58 @@
         align-center
         column
         justify-center
+        pt-5
+        mt-5
       >
         <v-avatar
           :tile="tile"
           :size="200"
         >
-          <img :src="profile" alt="avatar">
+          <img
+            :src="profile"
+            alt="avatar"
+          >
         </v-avatar>
-        <h1 class="display-2 font-weight-thin mb-3">{{ name }}</h1>
-        <h3 class="subheading">{{ title }}</h3>
+        <h1 class="display-2 font-weight-thin mb-3">
+          {{ name }}
+        </h1>
+        <h3 class="subheading">
+          {{ title }}
+        </h3>
+        <v-layout
+          row
+          wrap
+          text-xs-center
+        >
+          <v-flex
+            xs6
+            offset-xs3
+          >
+            <v-card
+              dark
+              flat
+              color="transparent"
+            >
+              <v-card-text class="px-0">
+                {{ intro }}
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex
+            xs6
+            offset-xs3
+          >
+            <v-card
+              dark
+              flat
+              color="transparent"
+            >
+              <v-icon>mdi-email</v-icon>
+              <br>
+              {{ email }}
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-layout>
     </v-parallax>
   </v-container>
@@ -28,7 +75,8 @@ export default {
   data: () => ({
     name: 'Kelina Shrestha',
     title: 'Ruby on Rails Engineer',
-    intro: "Self-motivated, dedicated and organized software engineer pursuing professional experience in technology with an aim to solve problems with technology. Collaborative team player uplifting performance of applications and team to achieve targeted goals accepting challenges.",
+    email: 'kelishrestha97@gmail.com',
+    intro: 'Self-motivated, dedicated and organized software engineer pursuing professional experience in technology with an aim to solve problems with technology. Collaborative team player uplifting performance of applications and team to achieve targeted goals accepting challenges.',
     bgimage: 'https://scontent.fktm3-1.fna.fbcdn.net/v/t1.0-9/32464466_10209916763430652_5090903458404368384_o.jpg?_nc_cat=103&_nc_ht=scontent.fktm3-1.fna&oh=5b20122aee9b6f22aca5b6e2e102a45d&oe=5D50A52E',
     profile: 'https://scontent.fktm3-1.fna.fbcdn.net/v/t31.0-8/22555553_10208674443253424_8742489099592047203_o.jpg?_nc_cat=102&_nc_ht=scontent.fktm3-1.fna&oh=a7e615831bf516f5d66f11541fc76db6&oe=5D0D31AC'
   }),
@@ -43,3 +91,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .v-parallax {
+    height: 100vh !important;
+  }
+</style>
